a78bec17012aee1eeabeefc622fd21d8b3bb9841
Agatha77 <jaram1204@gmail.com>
2022-02-23 09:31:37 +0900

    commit

--- mycode.R
+++ mycode.R
@@ -85,3 +85,50 @@ p <- ggplot (data = mpg,
 p + geom_boxplot(fill = "lightyellow")
 
 p + geom_boxplot(fill = "lightyellow") + coord_flip() # 눕히기
+
+
+#ggplot으로 geom_bar그리기
+
+p <- ggplot(data = diamonds,
+            mapping = aes(x = cut, fill = cut))
+
+p + geom_bar(show.legend = F, width = 1) #width는 barplot의 간격
+# 0.5로 두면 간격이 띄워짐
+
+#극좌표계
+p + geom_bar(show.legend = F, width = 1) +
+  labs(x = NULL, y = NULL) +
+  theme(aspect.ratio = 1) +
+  coord_polar()
+
+world <- map_data("world")
+ggplot(world, aes(long, lat, group = group)) + 
+  geom_polygon(fill = "lightyellow", color = "blue")
+
+nz <-map_data("nz")
+ggplot(world, aes(long, lat, group = group)) + 
+  geom_polygon(fill = "orange", color = "tomato")
+
+
+
+##-----------------------데이터 시각화 실습 (타이틀 넣기)----------------------
+
+library(ggplot2)
+p <- ggplot (data = mpg,aes(displ, hwy)) + #data생략가능
+  geom_point (aes(color = class)) +
+  geom_smooth (se = F)
+
+#labs => 타이틀추가
+p + labs(title = "Fuel Efficiency .vs. Engine Size",
+         subtitle = "Two seaters are exceptional", #서브타이틀
+         caption = "Data from fueleconomy.gov", #설명
+         x = "Engine displacement (L)", #x축
+         y = "Highway fuel economy", #y축
+         color = "Car Type") #유형별 색상
+
+
+##--------------------- r이 함수형 프로그램인 이유 -----------------------
+
+"+"(3,4)
+
+":" (1,10)
